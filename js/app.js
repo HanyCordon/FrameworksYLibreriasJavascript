f13bea6985735392601e3d00b8afc417d9a70d25
Hany Cordon <greenstefhany@yahoo.com>
2017-10-04 15:06:07 -0500

    caida de dulces, reiniciar
    caida de dulces, reiniciar

--- js/app.js
+++ js/app.js
@@ -2,12 +2,10 @@ var tiempoAtras;
 var min;
 var seg;
 var temporizador;
-//updateReloj();
+
 
 function updateReloj() {
 var number = document.getElementById('timer');
-
-
 var hora = 0;
 var minutos = 1-min;
 var segundos = 59-seg;
@@ -17,7 +15,6 @@ if (min==2) {
   tiempoAtras+= ":00";
   tiempoAtras+= ":00";
   FinalizarJuego();
-
 }
 else {
   tiempoAtras= (hora < 10) ? hora :hora;
@@ -36,16 +33,59 @@ number.innerHTML = tiempoAtras;
 }
 
 function CambiarColor(elemento){
-  $(elemento).animate({
-    color : "green"
-  },1000, function(){
-    $(this).animate(
-      {
-          color : "yellow"
-      }, 1000
-    )
-  });
+  var color = $(elemento).css("color");
+  if (color=="rgb(255, 255, 0)") {
+    $(elemento).css("color","green");
+  }
+  else {
+    $(elemento).css("color","yellow");
+  }
+  setTimeout(CambiarColor(".main-titulo"),1000) ;
 }
+
+/*function CaidaDeDulces(){
+  for (var i = 0; i < 7; i++) {
+    $(".col-1").append("<div></div>");
+    $(".col-1").child[i].append("<img></img>");
+    $("img").attr("src",function(){
+      var numero = Math.floor((Math.random() * 4) + 1);
+      var ruta = "image/"+numero+".png";
+      return ruta;
+    });
+  }*/
+
+function CaidaDeDulces(){
+  for (var i = 1; i <8; i++) {
+    for (var j = 0; j < 7; j++) {
+      var tipoDulce= Math.floor((Math.random() * 4) + 1);
+      var aux=document.createElement('img')
+      $(".col-"+i)[0].append(aux)
+      $(aux).addClass('elemento')
+      $(aux).draggable({
+      start: function(event,ui){
+      drag(event,ui)},
+      revert:"invalid",
+      helper:"clone"})
+      $(aux).droppable({
+      drop: function(event,ui){
+      drop(event,ui)}
+      });
+      $(aux).attr('src',"image/"+tipoDulce+".png")
+    }
+  }
+}
+
+function ReiniciarJuego() {
+  clearTimeout(temporizador);
+  $(".btn-reinicio").text("Iniciar");
+  $(".panel-tablero").show("slow");
+  $(".panel-score").animate(
+    {
+      width: "-=50"
+    }, 1000
+  );
+}
+
 
 function FinalizarJuego() {
   clearTimeout(temporizador);
@@ -64,12 +104,12 @@ $(".btn-reinicio").on("click", function(){
     clearTimeout(temporizador);
   }
   else {
-    $(".btn-reinicio").text("Iniciar");
-    clearTimeout(temporizador);
+    ReiniciarJuego();
   }
   min =0;
   seg =0;
   updateReloj();
+  CaidaDeDulces();
 });
 
-CambiarColor(".main-titulo");
+//CambiarColor(".main-titulo");
